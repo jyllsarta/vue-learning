2504fc8f2d5884946031b3ebcb7f0ffe7eb3231a
jyllsarta <jumpei.hosokawa@aktsk.jp>
2019-06-07 20:18:12 +0900

    トランジションのテスト

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,11 @@
 var app =  new Vue({
-    el: "#app"
-})
+    el: "#app",
+    methods: {
+        toggleShow: function(){
+            this.show = !this.show;
+        }
+    },
+    data: {
+        show: true
+    }
+});
