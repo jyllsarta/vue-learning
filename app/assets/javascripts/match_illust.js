fda6027c7555bf9f95c008f61ca8a8620569a61f
jyll sarta <illstarred.dive@gmail.com>
2019-06-16 18:02:47 +0900

    完成

--- app/assets/javascripts/match_illust.js
+++ app/assets/javascripts/match_illust.js
@@ -1,14 +1,69 @@
 var app = new Vue({
     el: '#app',
     data: {
-      blur: 0,
-      brightness: 0,
-      saturate: 0,
-      hueRotate: 0,
+      player:{
+        blur: 50,
+        brightness: 50,
+        saturate: 50,
+        hueRotate: 50,
+      },
+      target:{
+        blur: 50,
+        brightness: 50,
+        saturate: 50,
+        hueRotate: 50,
+      },
+      showingAnswer: false
+    },
+    created: function(){
+      this.target.blur = parseInt(Math.random() * 100);
+      this.target.brightness = parseInt(Math.random() * 100);
+      this.target.saturate = parseInt(Math.random() * 100);
+      this.target.hueRotate = parseInt(Math.random() * 100);
     },
     computed: {
-      filter: function(){
-        return `blur(${this.blur /10}px) brightness(${this.brightness / 50}) saturate(${this.saturate / 50}) hue-rotate(${this.hueRotate / 100 * 360 + 180}deg)`;
+      playerFilter: function(){
+        return `blur(${this.player.blur /10}px) brightness(${this.player.brightness / 50}) saturate(${this.player.saturate / 50}) hue-rotate(${this.player.hueRotate / 100 * 360 + 180}deg)`;
+      },
+      targetFilter: function(){
+        return `blur(${this.target.blur /10}px) brightness(${this.target.brightness / 50}) saturate(${this.target.saturate / 50}) hue-rotate(${this.target.hueRotate / 100 * 360 + 180}deg)`;
+      },
+      score: function(){
+        let diffs = 0;
+        for (const key of Object.keys(this.player)){
+          diffs += Math.abs(this.target[key] - this.player[key])
+        }
+        return 100 - diffs;
+      },
+      tweetingMessage: function(){
+        return `CSSのfilterプロパティ ${this.score}%理解した`;
       }
+    },
+    methods: {
+      showAnswer: function () {
+        this.showingAnswer = true;
+      },
+      grade: function(attributeName){
+        const diff = Math.abs(this.player[attributeName] - this.target[attributeName]);
+        if (diff <= 1){
+          return "◎";
+        }
+        if (diff <= 5){
+          return "○";
+        }
+        if (diff <= 15){
+          return "△";
+        }
+        return "×";
+      },
+      reload: function(){
+        location.reload();
+      },
+      tweet: function(){
+        const tweetContent = encodeURI(this.tweetingMessage);
+        const url = encodeURI("http://jyllsarta.net/match_illust")
+        const fullurl = `https://twitter.com/intent/tweet?url=${url}&text=${tweetContent}`;
+        window.open(fullurl);
+      },
     }
 });
